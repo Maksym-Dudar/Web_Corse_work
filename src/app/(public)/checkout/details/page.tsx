55303a68d20b1974e38ced167f32ecb593ac1488
Maksym Dudarchuk <129439508+Maksym-Dudar@users.noreply.github.com>
2026-05-19 11:18:08 +0300

    Update page.tsx

--- src/app/(public)/checkout/details/page.tsx
+++ src/app/(public)/checkout/details/page.tsx
@@ -1,20 +1,11 @@
 "use client"
 
 import { useCheckout } from "@/features/checkout/hook/useCheckout";
-import { CheckoutPage } from "@/features/checkout/ui/CheckoutPage";
-import PaymentProvider from "@/provider/stripe/StripeProvider";
 
 export default function Page() {
-		const {
-			orderData,
-		} = useCheckout();
-	console.log("orderData: ", orderData);
-	if (!orderData || !orderData?.id) return;
 	return (
 		<div>
-			<PaymentProvider orderId={orderData?.id}>
 				<CheckoutPage />
-			</PaymentProvider>
 		</div>
 	);
 }
